1d819e63df904c1aecc3c6590faa99d3269a94cf
sharyar <sharyar430@gmail.com>
2025-03-08 23:07:02 +0500

    absic structure created

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div>home</div>
+    <>
+    <main className='border-2 border-red-500 h-[100vh] '>
+<div className='border-2 border-blue-500 h-[75vh]'>
+    <div className='border-2 border-purple-500 h-[15%] flex justify-center items-center text-[3rem]'>
+       <h1>TODO TASK</h1>
+    </div>
+    <div className='border-2 border-purple-500 h-[85%]'>
+        addtodo
+    </div>
+</div>
+
+<div className='border-2 border-green-500 h-[25vh]'>
+    bottom
+</div>
+    </main>
+    
+    </>
   )
 }
 
